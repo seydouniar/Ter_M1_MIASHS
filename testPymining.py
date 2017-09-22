09dcae6733be0012ee4e208686dd8b812789817d
juicebox81 <eloanlagier@yahoo.fr>
2017-09-22 15:18:15 +0200

    test mise a jour ne marche pas avec utf-8..

--- testPymining.py
+++ testPymining.py
@@ -1,8 +1,12 @@
 from pymining import itemmining
-txt = open("/Users/Eloan/Desktop/Ter_M1_MIASHS/Sans_titre.txt").read()
+txt = open("Sans_titre.txt".encode("utf-8")).read()
+txt=str(txt)
 list_phrase=txt.split(".")
-print(list_phrase)
-transactions = (('a', 'b', 'c'), ('b'), ('a'), ('a', 'c', 'd'), ('b', 'c'), ('b', 'c'))
-relim_input = itemmining.get_relim_input(transactions)
-report = itemmining.relim(relim_input, min_support=2)
+List_phrases=[]
+for i in range(len(list_phrase)-2):
+    List_phrases.append(list_phrase[i].split(" "))
+    List_phrases[i]
+relim_input = itemmining.get_relim_input(List_phrases)
+report = itemmining.relim(relim_input, min_support=6)
 report
+print(report)
