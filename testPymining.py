c89bf392c30de86401020c5d8c6872e9e1d65bf5
seydouniar <seydouniar@gmail.com>
2017-09-24 20:50:20 +0200

    Update testPymining.py

--- testPymining.py
+++ testPymining.py
@@ -1,5 +1,4 @@
 from pymining import itemmining
-<<<<<<< HEAD
 txt = open("Sans_titre.txt".encode("utf-8")).read()
 print(txt)
 txt=str(txt)
@@ -15,12 +14,3 @@ report = itemmining.relim(relim_input, min_support=12)
 
 print(len(report))
 print(report)
-=======
-txt = open("/Users/Eloan/Desktop/Ter_M1_MIASHS/Sans_titre.txt").read()
-list_phrase=txt.split(".")
-print(list_phrase)
-transactions = (('a', 'b', 'c'), ('b'), ('a'), ('a', 'c', 'd'), ('b', 'c'), ('b', 'c'))
-relim_input = itemmining.get_relim_input(transactions)
-report = itemmining.relim(relim_input, min_support=2)
-report
->>>>>>> 0a8458650e91173df5ade9335162726559ab443e
